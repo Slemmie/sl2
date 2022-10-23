3356b759f37cc314fb22343e6b33a0f410f7ef02
Slemmie <thorvejeneriksen@gmail.com>
2022-10-23 08:02:20 +0200

    template.cpp files

---
+++ template/c++20/template.cpp
@@ -0,0 +1,44 @@
+// hilo
+
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+void prep(const int numt);
+
+void test_case(const int curt);
+
+constexpr const bool is_multi_tests = false;
+
+int main() {
+	std::ios::sync_with_stdio(0); std::cin.tie(0);
+	
+	if constexpr (is_multi_tests) {
+		int t;
+		std::cin >> t;
+		prep(t);
+		for (int i = 1; i <= t; i++) {
+			test_case(t);
+		}
+	} else {
+		prep(1);
+		test_case(0);
+	}
+	
+	return 0;
+}
+
+void prep(const int numt) {
+	
+}
+
+void test_case(const int curt) {
+	
+	
+	
+}
